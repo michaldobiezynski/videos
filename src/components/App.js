f1d2846e3fc6da9d3134068566bf4b4939f30d65
michaldobiezynski <dobiezynski@gmail.com>
2020-02-26 19:00:14 +0000

    defaulting video selection

--- src/components/App.js
+++ src/components/App.js
@@ -6,39 +6,49 @@ import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
-	state = { videos: [], selectedVideo: null };
+  state = { videos: [], selectedVideo: null };
 
-	onTermSubmit = async term => {
-		const response = await youtube.get('/search', {
-			params: {
-				q: term,
-			},
-		});
+  componentDidMount() {
+    this.onTermSubmit('buildings');
+  }
 
-		this.setState({ videos: response.data.items });
-	};
+  onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term,
+      },
+    });
 
-	onVideoSelect = video => {
-		this.setState({ selectedVideo: video });
-	};
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
 
-	render() {
-		return (
-			<div className='ui container'>
-				<SearchBar onTermSubmit={this.onTermSubmit} />
-				<div className='ui grid'>
-					<div className='ui row'>
-						<div className='eleven wide column'>
-							<VideoDetail video={this.state.selectedVideo} />
-						</div>
-						<div className='five wide column'>
-							<VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	}
+  onVideoSelect = video => {
+    this.setState({ selectedVideo: video });
+  };
+
+  render() {
+    return (
+      <div className='ui container'>
+        <SearchBar onTermSubmit={this.onTermSubmit} />
+        <div className='ui grid'>
+          <div className='ui row'>
+            <div className='eleven wide column'>
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className='five wide column'>
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
