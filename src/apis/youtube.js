6a9ac265b4e43294ca64924620c095721c4234fe
Michal Dobiezynski <dobiezynski@gmail.com>
2020-02-26 09:03:15 +0000

    searching for videos

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyC3cF-RVg9axGI0oScsm5Vup1tSUwBIwuo';
+
+export default axios.create({
+	baseURL: 'https://www.googleapis.com/youtube/v3',
+	params: {
+		part: 'snippet',
+		maxResults: 5,
+		key: KEY,
+	},
+});
